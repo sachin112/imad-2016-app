6376b6642c2d39cff8fa799a6a3f83e67efb3a4f
sachin112 <v.sachin.15csc@bml.edu.in>
2016-09-22 00:09:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 app.get('/article-one', function(req,res)
 {
-    res.sendfile('Article one requested and will be served');
+   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 }
 );
 app.get('/article-two', function(req,res)
